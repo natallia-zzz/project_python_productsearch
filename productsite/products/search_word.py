8e92dcd5ebeed5b9ea1211cc538acf382f1dbcbc
nastassya-sav <61316882+nastassya-sav@users.noreply.github.com>
2020-05-17 15:41:09 +0300

    interactive

--- productsite/products/search_word.py
+++ productsite/products/search_word.py
@@ -2,12 +2,18 @@ def is_part_in_list(str_, words):
     for word in words:
         if str_.lower() in word.lower():
             return word
+    return "Nothing is found"
 
 
 def main():
-    words = ["bags", "shoes", "coats"]
-    str_ = input("Введите слово для проверки\n")
-    print(is_part_in_list(str_, words))
+    str_ = 0
+    while str_ != 'exit':
+        words = ["bags chanel", "shoes gucci", "coats versace"]
+        str_ = input("Введите слово для проверки или exit, чтобы выйти\n")
+        if str_ == 'exit':
+            break
+        else:
+            print(is_part_in_list(str_, words))
 
 
 if __name__ == "__main__":
