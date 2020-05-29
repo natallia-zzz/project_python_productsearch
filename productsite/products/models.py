5560ae16040112f5b159286c3a0426c1d328d22c
Natallia Zhamaitsiak <61240842+natallia-zzz@users.noreply.github.com>
2020-05-29 13:02:01 +0300

    no message

--- productsite/products/models.py
+++ productsite/products/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
 # Create your models here.
-# class Result(models.Model):
-   
+class Product(models.Model):
+   pr_id = models.CharField(max_length=30)
+   pr_title = models.CharField(max_length=30)
+   pr_description =  models.CharField(max_length=100)
+   pr_manufacturer = models.CharField(max_length=30)
+   pr_price = models.DecimalField(max_digits=8, decimal_places=2)
