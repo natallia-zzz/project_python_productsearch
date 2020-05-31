ed929a4916a43d4bdfd810a9576af2777ac1ddfd
Natallia Zhamaitsiak <61240842+natallia-zzz@users.noreply.github.com>
2020-05-31 13:31:55 +0300

    no message

--- productsite/products/models.py
+++ productsite/products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-   pr_id = models.CharField('id',max_length=30)
+   pr_id = models.CharField('id',primary_key = True,max_length=30)
    pr_title = models.CharField('title',max_length=100)
    pr_description =  models.CharField('description',max_length=15000)
    pr_manufacturer = models.CharField('manufacturer',max_length=30)
