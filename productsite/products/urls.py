ef94e98f2cc3dc83170391dd9a673737ad9fae22
nastassya-sav <savasya08@gmail.com>
2020-05-31 22:11:27 +0300

    creating login

--- productsite/products/urls.py
+++ productsite/products/urls.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls import url
 from . import views
-from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView
 
 app_name = 'products'
 urlpatterns = [
     path('', views.HomePageView.as_view(), name = 'home'),
     path('results/', views.ResultsView.as_view(), name = 'results'),
     url(r'^signup/$', views.signup, name='signup'),
-    url(r'^login/$', auth_views.login, name='login'),
+    path('login/', LoginView.as_view(template_name='products/login.html'), name="login"),
 ]
--- productsite/productsite/settings.py
+++ productsite/productsite/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -120,4 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = '/products'
