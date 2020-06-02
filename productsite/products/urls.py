1fa65221942c8c3f2e2a5c56ac241661881fd09f
Natallia Zhamaitsiak <61240842+natallia-zzz@users.noreply.github.com>
2020-06-02 11:30:48 +0300

    no message

--- productsite/products/urls.py
+++ productsite/products/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='products/login.html'), name="login"),
     path('<pr_id>/', views.ProductView.as_view(), name='details'),
     path('logout', LogoutView.as_view(), name='logout'),
-    path('checkout/', views.CheckoutView.as_view(), name = 'checkout')
+    url(r'^add/$', views.addbin, name = 'add'),
+    path('checkout/', views.CheckoutView.as_view(), name = 'checkout'),
 ]
--- productsite/products/views.py
+++ productsite/products/views.py
@@ -1,30 +1,33 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
-from .models import Product
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Product, Basket
 from django.views import generic
 from django.db.models import Q
 from django.contrib.auth import login, authenticate, mixins
 from django.contrib.auth.forms import UserCreationForm
 from . import search_algorithm_strict as test
-
+from django.urls import reverse
 
 # Create your views here.
 class HomePageView(generic.TemplateView):
     template_name = 'products/home.html'
-    def get_context_data(self, **kwargs):
-# Call the base implementation first to get a context
-        c = super().get_context_data(**kwargs)
-        user = self.request.user
-        return {'user':user}
+    # def get_context_data(self, **kwargs):
+    # c = super().get_context_data(**kwargs)
+    # user = self.request.user
+    # return {'user':user}
+
 
 class ResultsView(generic.ListView):
     model = Product
     template_name = 'products/results.html'
     context_object_name = 'results'
+
     def get_queryset(self):
         query = self.request.GET.get('res')
-        result = Product.objects.filter(Q(pr_title__icontains=query)|Q(pr_description__icontains=query)|Q(pr_description__icontains=query))
+        result = Product.objects.filter(
+            Q(pr_title__icontains=query) | Q(pr_description__icontains=query) | Q(pr_description__icontains=query))
         return result.order_by('pr_price')
+
 
 def signup(request):
     if request.method == 'POST':
@@ -40,6 +43,7 @@ def signup(request):
         form = UserCreationForm()
     return render(request, 'products/signup.html', {'form': form})
 
+
 class ProductView(generic.TemplateView):
     model = Product
     template_name = 'products/detail.html'
@@ -47,7 +51,18 @@ class ProductView(generic.TemplateView):
         context = get_object_or_404(Product, pk=self.kwargs['pr_id'])
         return {'product': context}
 
-class CheckoutView(mixins.LoginRequiredMixin, generic.DetailView):
-    context_object_name = 'checkout'
-    login_url = '/products/login/'
+
+class CheckoutView(mixins.LoginRequiredMixin, generic.TemplateView):
+    model = Product
+    redirect_field_name = 'products:home'
     template_name = 'products/basket.html'
+
+
+def addbin(request, pr_id):
+    product = get_object_or_404(Product, pk=pr_id)
+    user = request.user
+    num = request.POST['num']
+    item = Basket.object.create(prod=product.pr_id, user=user.id, num=num)
+    return HttpResponseRedirect(reverse('products:checkout', kwargs=(item,)))
+
+
